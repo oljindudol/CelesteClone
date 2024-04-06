59114aafad91f7fc4a00474478a926294b867d6f
lee <lee.jinyoung.k181122@gmail.com>
2024-04-06 14:37:10 +0900

    #6 파티클UI만들기 완료

--- Project/Client/ParticleSystemUI.cpp
+++ Project/Client/ParticleSystemUI.cpp
@@ -24,9 +24,9 @@ void ParticleSystemUI::render_update()
 	tParticleModule CurModule = pPS->GetModule();
 	tParticleModule NewModule = CurModule;
 
-	ImGui::Separator();
 	// Drag Module
-	ImGui::Text("Drag Module : "); ImGui::SameLine();
+	ImGui::Separator();
+	ImGui::Text("Drag Module"); ImGui::SameLine();
 	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::DRAG] )
 	{
 		if (ImGui::Button("On##DragModule"))
@@ -44,9 +44,11 @@ void ParticleSystemUI::render_update()
 	ImGui::Text("DragTime"); ImGui::SameLine();
 	ImGui::DragFloat("##DragTime", &NewModule.DragTime, 0.01f, 0.01f, 10000.f);
 
-	ImGui::Separator();
+
+
 	// Spawn Module
-	ImGui::Text("Spawn Module : "); ImGui::SameLine();
+	ImGui::Separator();
+	ImGui::Text("Spawn Module"); ImGui::SameLine();
 	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SPAWN]) {
 		if (ImGui::Button("On##SpawnModule")) {
 			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SPAWN] = 0;
@@ -57,8 +59,8 @@ void ParticleSystemUI::render_update()
 			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SPAWN] = 1;
 		}
 	}
-	ImGui::Text("SpaceType : "); ImGui::SameLine();
-	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SPAWN]) {
+	ImGui::Text("SpaceType"); ImGui::SameLine();
+	if (1 == CurModule.SpaceType) {
 		if (ImGui::Button("WorldSpace")) {
 			NewModule.SpaceType = 0;
 		}
@@ -68,45 +70,172 @@ void ParticleSystemUI::render_update()
 			NewModule.SpaceType = 1;
 		}
 	}
-
-
-	CurModule.vSpawnColor = Vec4(0.2f, 0.4f, 0.9f, 1.f);
-	CurModule.vSpawnMinScale = Vec4(50.f, 25.f, 1.f, 1.f);
-	CurModule.vSpawnMaxScale = Vec4(50.f, 25.f, 1.f, 1.f);
-	CurModule.MinLife = 5.f;
-	CurModule.MaxLife = 5.f;
-	CurModule.MinMass = 1.f;
-	CurModule.MaxMass = 1.f;
-	CurModule.SpawnShape = 1; // 0 : Sphere, 1 : Box
-	CurModule.Radius = 100.f;
-	CurModule.vSpawnBoxScale = Vec4(10.f, 10.f, 0.f, 0.f);
-	CurModule.SpawnRate = 50;
+	ImGui::Text("Spawn Color"); ImGui::SameLine();
+	ImGui::ColorEdit4("##Spawn Color", &NewModule.vSpawnColor.x);
+	ImGui::Text("Min Scale"); ImGui::SameLine();
+	ImGui::DragFloat4("##Min Scale", &NewModule.vSpawnMinScale.x, 0.1f, 0.0f, 1000.0f);
+	ImGui::Text("Max Scale"); ImGui::SameLine();
+	ImGui::DragFloat4("##Max Scale", &NewModule.vSpawnMaxScale.x, 0.1f, 0.0f, 1000.0f);
+	ImGui::Text("Min Life"); ImGui::SameLine();
+	ImGui::DragFloat("##Min Life", &NewModule.MinLife, 0.1f, 0.0f, 100.0f);
+	ImGui::Text("Max Life"); ImGui::SameLine();
+	ImGui::DragFloat("##Max Life", &NewModule.MaxLife, 0.1f, 0.0f, 100.0f);
+	ImGui::Text("Min Mass"); ImGui::SameLine();
+	ImGui::DragFloat("##Min Mass", &NewModule.MinMass, 0.1f, 0.0f, 100.0f);
+	ImGui::Text("Max Mass"); ImGui::SameLine();
+	ImGui::DragFloat("##Max Mass", &NewModule.MaxMass, 0.1f, 0.0f, 100.0f);
+	ImGui::Text("Spawn Shape"); ImGui::SameLine();
+	if (1 == CurModule.SpawnShape) {
+		if (ImGui::Button("Box")) {
+			NewModule.SpawnShape = 0;
+		}
+	}
+	else {
+		if (ImGui::Button("Sphere")) {
+			NewModule.SpawnShape = 1;
+		}
+	}
+	ImGui::Text("Radius"); ImGui::SameLine();
+	ImGui::DragFloat("##Radius", &NewModule.Radius, 1.0f, 0.0f, 1000.0f);
+	ImGui::Text("Box Scale"); ImGui::SameLine();
+	ImGui::DragFloat4("##Box Scale", &NewModule.vSpawnBoxScale.x, 0.1f, 0.0f, 100.0f);
+	ImGui::Text("Spawn Rate"); ImGui::SameLine();
+	ImGui::DragInt("##Spawn Rate", &NewModule.SpawnRate, 1, 0, 1000);
 
 	// Add Velocity Module
-	CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::ADD_VELOCITY] = 1;
-	CurModule.AddVelocityType = 0; // 0 : From Center, 1: To Center, 2: Fix Direction
-	CurModule.MinSpeed = 500;
-	CurModule.MaxSpeed = 500;
-	CurModule.FixedDirection;
-	CurModule.FixedAngle;
+	ImGui::Separator();
+	ImGui::Text("Add Velocity Module"); ImGui::SameLine();
+	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::ADD_VELOCITY]) {
+		if (ImGui::Button("On##AddVelocityModule")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::ADD_VELOCITY] = 0;
+		}
+	}
+	else {
+		if (ImGui::Button("Off##AddVelocityModule")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::ADD_VELOCITY] = 1;
+		}
+	}
+	ImGui::Text("AddVelocityType"); ImGui::SameLine();
+	if (0 == CurModule.AddVelocityType) {
+		if (ImGui::Button("From Center")) {
+			NewModule.AddVelocityType = 1;
+		}
+	}
+	else if(1 == CurModule.AddVelocityType) {
+		if (ImGui::Button("To Center")) {
+			NewModule.AddVelocityType = 2;
+		}
+	}
+	else
+	{
+		if (ImGui::Button("Fix Direction")) {
+			NewModule.AddVelocityType = 0;
+		}
+	}
+	ImGui::Text("Min Speed"); ImGui::SameLine();
+	ImGui::DragFloat("##Min Speed", &NewModule.MinSpeed, 1, 0, 1000);
+	ImGui::Text("Max Speed"); ImGui::SameLine();
+	ImGui::DragFloat("##Max Speed", &NewModule.MaxSpeed, 1, 0, 1000);
+	ImGui::Text("FixedDirection"); ImGui::SameLine();
+	ImGui::DragFloat4("##FixedDirection", &NewModule.FixedDirection.x, 0.1, 0, 1000);
+	ImGui::Text("FixedAngle"); ImGui::SameLine();
+	ImGui::DragFloat("##FixedAngle", &NewModule.FixedAngle, 0.1, 0, 1000);
 
-	// Scale
-	CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SCALE] = 0;
-	CurModule.vScaleRatio = Vec3(0.1f, 0.1f, 0.1f);
+	// Scale Module
+	ImGui::Separator();
+	ImGui::Text("Scale Module"); ImGui::SameLine();
+	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SCALE]) {
+		if (ImGui::Button("On##ScaleModule")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SCALE] = 0;
+		}
+	}
+	else {
+		if (ImGui::Button("Off##ScaleModule")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::SCALE] = 1;
+		}
+	}
+	ImGui::Text("Scale Ratio"); ImGui::SameLine();
+	ImGui::DragFloat3("##Scale Ratio", &NewModule.vScaleRatio.x, 0.01f, 0.0f, 10.0f);
 
-	// Noise Force
-	CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::NOISE_FORCE] = 1;
-	CurModule.NoiseForceScale = 10.f;
-	CurModule.NoiseForceTerm = 0.3f;
+
+	// Noise Force// Noise Force
+	ImGui::Separator();
+	ImGui::Text("Noise Force"); ImGui::SameLine();
+	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::NOISE_FORCE]) {
+		if (ImGui::Button("On##NoiseForce")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::NOISE_FORCE] = 0;
+		}
+	}
+	else {
+		if (ImGui::Button("Off##NoiseForce")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::NOISE_FORCE] = 1;
+		}
+	}
+	ImGui::Text("NoiseForceScale"); ImGui::SameLine();
+	ImGui::DragFloat("##NoiseForceScale", &NewModule.NoiseForceScale, 0.1f, 0.0f, 100.0f);
+	ImGui::Text("NoiseForceTerm"); ImGui::SameLine();
+	ImGui::DragFloat("##NoiseForceTerm", &NewModule.NoiseForceTerm, 0.01f, 0.0f, 1.0f);
 
 	// Calculate Force
-	CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::CALCULATE_FORCE] = 1;
-	
-	// Render 
-	CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::RENDER] = 1;
-	CurModule.VelocityAlignment = 1; // 속도에 따른 방향 정렬
-	CurModule.AlphaBasedLife = 1; // 0 : off, 1 : NomrlizedAge, 2: Age
-	CurModule.AlphaMaxAge = 2.f;
+	ImGui::Separator();
+	ImGui::Text("Calculate Force"); ImGui::SameLine();
+	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::CALCULATE_FORCE]) {
+		if (ImGui::Button("On##CalculateForce")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::CALCULATE_FORCE] = 0;
+		}
+	}
+	else {
+		if (ImGui::Button("Off##CalculateForce")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::CALCULATE_FORCE] = 1;
+		}
+	}
+
+	// Render
+	ImGui::Separator();
+	ImGui::Text("Render"); ImGui::SameLine();
+	if (1 == CurModule.arrModuleCheck[(UINT)PARTICLE_MODULE::RENDER]) {
+		if (ImGui::Button("On##Render")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::RENDER] = 0;
+		}
+	}
+	else {
+		if (ImGui::Button("Off##Render")) {
+			NewModule.arrModuleCheck[(UINT)PARTICLE_MODULE::RENDER] = 1;
+		}
+	}
+	ImGui::Text("VelocityAlignment"); ImGui::SameLine();
+	if (0 == CurModule.VelocityAlignment) {
+		if (ImGui::Button("Off")) {
+			NewModule.VelocityAlignment = 1;
+		}
+	}
+	else if (1 == CurModule.VelocityAlignment) {
+		if (ImGui::Button("On")) {
+			NewModule.VelocityAlignment = 0;
+		}
+	}
+
+
+	ImGui::Text("AlphaBasedLife"); ImGui::SameLine();
+	if (0 == CurModule.AlphaBasedLife) {
+		if (ImGui::Button("Off")) {
+			NewModule.AlphaBasedLife = 1;
+		}
+	}
+	else if (1 == CurModule.AlphaBasedLife) {
+		if (ImGui::Button("NomrlizedAge")) {
+			NewModule.AlphaBasedLife = 2;
+		}
+	}
+	else
+	{
+		if (ImGui::Button("Age")) {
+			NewModule.AlphaBasedLife = 0;
+		}
+	}
+	ImGui::Text("AlphaMaxAge"); ImGui::SameLine();
+	ImGui::DragFloat("##AlphaMaxAge", &NewModule.AlphaMaxAge, 0.1f, 0.0f, 10.0f);
+
 
 
 	pPS->SetModule(NewModule);
